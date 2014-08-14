0cba7991462d45bd92eab0a87d140003802fb6ef
Luis Pedro Coelho <luis@luispedro.org>
2014-08-14 22:23:19 +0200

    DOC Better structure. Better documentation

--- ch10/simple_classification.py
+++ ch10/simple_classification.py
@@ -16,27 +16,45 @@ basedir = '../SimpleImageDataset/'
 
 
 def features_for(im):
+    '''Compute features for an image
+
+    Parameters
+    ----------
+    im : str
+        filepath for image to process
+
+    Returns
+    -------
+    fs : ndarray
+        1-D array of features
+    '''
     im = mh.imread(im, as_grey=True).astype(np.uint8)
     return mh.features.haralick(im).mean(0)
 
-features = []
+haralicks = []
 sobels = []
 labels = []
-images = glob('{}/*.jpg'.format(basedir))
-for im in images:
-    features.append(features_for(im))
-    sobels.append(edginess_sobel(mh.imread(im, as_grey=True)))
-    labels.append(im[:-len('00.jpg')])
 
-features = np.array(features)
+# Use glob to get all the images
+images = glob('{}/*.jpg'.format(basedir))
+for fname in images:
+    haralicks.append(features_for(fname))
+    sobels.append(edginess_sobel(mh.imread(fname, as_grey=True)))
+    labels.append(fname[:-len('00.jpg')])
+
+haralicks = np.array(haralicks)
+sobels = np.array(sobels)
 labels = np.array(labels)
 
+# We use logistic regression because it is very fast.
+# Feel free to experiment with other classifiers
 scores = cross_validation.cross_val_score(
-    LogisticRegression(), features, labels, cv=5)
+    LogisticRegression(), haralicks, labels, cv=5)
 print('Accuracy (5 fold x-val) with Logistic Regrssion [std features]: {}%'.format(
     0.1 * round(1000 * scores.mean())))
 
+haralick_plus_sobel = np.hstack([np.atleast_2d(sobels).T, haralicks])
 scores = cross_validation.cross_val_score(
-    LogisticRegression(), np.hstack([np.atleast_2d(sobels).T, features]), labels, cv=5).mean()
+    LogisticRegression(), haralick_plus_sobel, labels, cv=5).mean()
 print('Accuracy (5 fold x-val) with Logistic Regrssion [std features + sobel]: {}%'.format(
     0.1 * round(1000 * scores.mean())))
