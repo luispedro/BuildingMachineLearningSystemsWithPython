4e347821462ab4963581ece843f3e544a9f895f9
Luis Pedro Coelho <luis@luispedro.org>
2014-08-16 17:12:19 +0200

    ENH Use sklearn for figure 1

--- ch07/figure1.py
+++ ch07/figure1.py
@@ -7,24 +7,34 @@
 
 import numpy as np
 from sklearn.datasets import load_boston
-import pylab as plt
+from sklearn.linear_model import LinearRegression
+from matplotlib import pyplot as plt
 from mpltools import style
 style.use('ggplot')
 
 boston = load_boston()
+
+# Index number five in the number of rooms
 plt.scatter(boston.data[:, 5], boston.target)
-plt.xlabel("RM")
+plt.xlabel("Number of rooms (RM)")
 plt.ylabel("House Price")
 
-
 x = boston.data[:, 5]
-x = np.array([[v] for v in x])
+# fit (used below) takes a two-dimensional array as input. We use np.atleast_2d
+# to convert from one to two dimensional, then transpose to make sure that the
+# format matches:
+x = np.transpose(np.atleast_2d(x))
+
 y = boston.target
 
-slope, res, _, _ = np.linalg.lstsq(x, y)
+lr = LinearRegression(fit_intercept=False)
+
+lr.fit(x, y)
+
 plt.plot([0, boston.data[:, 5].max() + 1],
-         [0, slope * (boston.data[:, 5].max() + 1)], '-', lw=4)
+         [0, lr.predict(boston.data[:, 5].max() + 1)], '-', lw=4)
 plt.savefig('Figure1.png', dpi=150)
 
-rmse = np.sqrt(res[0] / len(x))
-print('Residual: {}'.format(rmse))
+# The instance member `residues_` contains the sum of the squared residues
+rmse = np.sqrt(lr.residues_ / len(x))
+print('RMSE: {}'.format(rmse))
