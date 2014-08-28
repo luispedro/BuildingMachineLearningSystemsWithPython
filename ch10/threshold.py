80aceef4907eef8c8d29e202716b67b552d6773a
Luis Pedro Coelho <luis@luispedro.org>
2014-08-28 22:26:26 +0200

    DOC More comments & explanatory output

--- ch10/threshold.py
+++ ch10/threshold.py
@@ -7,15 +7,28 @@
 
 import numpy as np
 import mahotas as mh
+
+# Load our example image:
 image = mh.imread('../SimpleImageDataset/building05.jpg')
+
+# Convert to greyscale
 image = mh.colors.rgb2gray(image, dtype=np.uint8)
+
+# Compute a threshold value:
 thresh = mh.thresholding.otsu(image)
-print(thresh)
+print('Otsu threshold is {0}'.format(thresh))
+
+# Compute the thresholded image
 otsubin = (image > thresh)
+print('Saving thresholded image (with Otsu threshold) to otsu-threshold.jpeg')
 mh.imsave('otsu-threshold.jpeg', otsubin.astype(np.uint8) * 255)
-otsubin = ~ mh.close(~otsubin, np.ones((15, 15)))
+
+# Execute morphological opening to smooth out the edges
+otsubin = mh.open(otsubin, np.ones((15, 15)))
 mh.imsave('otsu-closed.jpeg', otsubin.astype(np.uint8) * 255)
 
+# An alternative thresholding method:
 thresh = mh.thresholding.rc(image)
-print(thresh)
+print('Ridley-Calvard threshold is {0}'.format(thresh))
+print('Saving thresholded image (with Ridley-Calvard threshold) to rc-threshold.jpeg')
 mh.imsave('rc-threshold.jpeg', (image > thresh).astype(np.uint8) * 255)
