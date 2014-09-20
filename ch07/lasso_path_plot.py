b001ea30efdd0d047cb8a3953cc9b9b652a8b1f4
Luis Pedro Coelho <luis@luispedro.org>
2014-09-20 23:38:24 +0200

    ENH Lasso path plot

---
+++ ch07/lasso_path_plot.py
@@ -0,0 +1,28 @@
+# This code is supporting material for the book
+# Building Machine Learning Systems with Python
+# by Willi Richert and Luis Pedro Coelho
+# published by PACKT Publishing
+#
+# It is made available under the MIT License
+
+from sklearn.linear_model import Lasso
+from sklearn.datasets import load_boston
+from matplotlib import pyplot as plt
+import numpy as np
+
+boston = load_boston()
+x = boston.data
+y = boston.target
+
+las = Lasso(normalize=1)
+alphas = np.logspace(-5, 2, 1000)
+_, coefs, _= las.path(x, y, alphas=alphas)
+
+fig,ax = plt.subplots()
+ax.plot(alphas, coefs.T)
+ax.set_xscale('log')
+ax.set_xlim(alphas.max(), alphas.min())
+ax.set_xlabel('Alpha')
+ax.set_ylabel('Coefficient weight')
+fig.savefig('Figure_LassoPath.png', dpi=150)
+
