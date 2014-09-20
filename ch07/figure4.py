045d3a13ee8e3fa79f13c8319bbdc0bd5ace5036
Luis Pedro Coelho <luis@luispedro.org>
2014-09-20 23:38:05 +0200

    ENH Print metrics

--- ch07/figure4.py
+++ ch07/figure4.py
@@ -8,9 +8,10 @@
 
 # This script plots prediction-vs-actual on training set for the Boston dataset
 # using OLS regression
-
+import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.datasets import load_boston
+from sklearn.metrics import mean_squared_error
 import pylab as plt
 
 boston = load_boston()
@@ -21,6 +22,8 @@ y = boston.target
 lr = LinearRegression()
 lr.fit(x, y)
 p = lr.predict(x)
+print("RMSE: {:.2}.".format(np.sqrt(mean_squared_error(y, p))))
+print("R2: {:.2}.".format(lr.score(x, y)))
 plt.scatter(p, y)
 plt.xlabel('Predicted price')
 plt.ylabel('Actual price')
