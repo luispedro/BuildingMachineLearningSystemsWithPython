268077983fece60f29d5e0a2cecf42866b5dcf9b
Luis Pedro Coelho <luis@luispedro.org>
2014-11-03 21:34:51 +0100

    ENH Add association rule code

--- ch08/apriori/apriori_naive.py
+++ ch08/apriori/apriori_naive.py
@@ -26,19 +26,23 @@ valid = set(el for el, c in counts.items() if (c >= minsupport))
 dataset = [[el for el in ds if (el in valid)] for ds in dataset]
 
 # Convert to frozenset for fast processing
-dataset = [frozenset(ds) for ds in dataset if len(ds) > 1]
+dataset = [frozenset(ds) for ds in dataset]
 
 itemsets = [frozenset([v]) for v in valid]
-freqsets = [itemsets]
+freqsets = itemsets[:]
 for i in range(16):
     print("At iteration {}, number of frequent baskets: {}".format(
         i, len(itemsets)))
     nextsets = []
+    tested = set()
     for it in itemsets:
         for v in valid:
             if v not in it:
                 # Create a new candidate set by adding v to it
                 c = (it | frozenset([v]))
+                if c in tested:
+                    continue
+                tested.add(c)
 
                 # Count support by looping over dataset
                 # This step is slow.
@@ -51,3 +55,30 @@ for i in range(16):
     if not len(itemsets):
         break
 print("Finished!")
+
+
+def rules_from_itemset(itemset, dataset, minlift=1.):
+    nr_transactions = float(len(dataset))
+    for item in itemset:
+        consequent = frozenset([item])
+        antecedent = itemset-consequent
+        base = 0.0
+        # acount: antecedent count
+        acount = 0.0
+
+        # ccount : consequent count
+        ccount = 0.0
+        for d in dataset:
+          if item in d: base += 1
+          if d.issuperset(itemset): ccount += 1
+          if d.issuperset(antecedent): acount += 1
+        base /= nr_transactions
+        p_y_given_x = ccount/acount
+        lift = p_y_given_x / base
+        if lift > minlift:
+            print('Rule {0} ->  {1} has lift {2}'
+                  .format(antecedent, consequent,lift))
+
+for itemset in freqsets:
+    if len(itemset) > 1:
+        rules_from_itemset(itemset, dataset, minlift=4.)
