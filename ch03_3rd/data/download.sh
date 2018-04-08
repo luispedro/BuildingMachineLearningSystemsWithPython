7235c997934f4e92e857b93d695389fd8a6b1dd6
Luis Pedro Coelho <luis@luispedro.org>
2018-04-08 18:55:45 +0200

    ENH Convert regression chapter to Jupyter

---
+++ ch03_3rd/data/download.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+curl -O https://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/regression/E2006.train.bz2
+bunzip2 E2006.train.bz2
+
--- ch07/data/download.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-curl -O http://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/regression/E2006.train.bz2
-bunzip2 E2006.train.bz2
