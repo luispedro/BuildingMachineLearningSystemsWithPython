623b537ec7b252631bf2fe31562d1c1e7fba1ed7
Matthieu Brucher <matthieu.brucher@gmail.com>
2018-06-06 21:59:21 +0100

    Adding Tensorflow simple classification example

--- ch04_3rd/data/download.sh
+++ ch04_3rd/data/download.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl -0 https://ia800107.us.archive.org/27/items/stackexchange/stackoverflow.com-Posts.7z
+curl -O https://ia800107.us.archive.org/27/items/stackexchange/stackoverflow.com-Posts.7z
 
 p7zip -d stackoverflow.com-Posts.7z
