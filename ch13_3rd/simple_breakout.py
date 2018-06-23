0eaefb381047c64c40f3190abffb9901b5995b17
Matthieu Brucher <matthieu.brucher@gmail.com>
2018-06-23 16:39:16 +0100

    typos

--- ch13_3rd/simple_breakout.py
+++ ch13_3rd/simple_breakout.py
@@ -10,7 +10,7 @@ env.render()
 
 is_done = False
 while not is_done:
-  # Perform a random action, returns the new frame, reward and whether the game is over
-  frame, reward, is_done, _ = env.step(env.action_space.sample())
-  # Render
-  env.render()
+    # Perform a random action, returns the new frame, reward and whether the game is over
+    frame, reward, is_done, _ = env.step(env.action_space.sample())
+    # Render
+    env.render()
